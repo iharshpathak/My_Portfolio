2c7c475e8e0df7d2a973d6de5c496b5e02788bf4
Harsh Pathak <83153262+iharshpathak@users.noreply.github.com>
2026-06-12 12:36:07 +0530

    Updated Hero.jsx

--- my-portfolio/src/components/Hero.jsx
+++ my-portfolio/src/components/Hero.jsx
@@ -33,7 +33,7 @@ const Hero =()=>{
           <div className='flex flex-col items-center lg:items-start'>
             <motion.h1 variants={container(0.5)} initial="hidden" animate="visible" className=' pb-16 text-5xl font-thin tracking-tight lg:mt-16 lg:text-8xl'>Harsh Pathak</motion.h1>
             <motion.span variants={container(1)} initial="hidden" animate="visible" className='bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent'>
-              Full Stack Developer
+              Full Stack Software Engineer
             </motion.span>
             <motion.p variants={container(1.50)} initial="hidden" animate="visible" className='my-2 max-w-xl py-6 font-light tracking-tighter'>{HERO_CONTENT}</motion.p>
           </div>
